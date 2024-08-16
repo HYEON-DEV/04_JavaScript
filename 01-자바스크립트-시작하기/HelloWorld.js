f2b57b28f756b501014c081595b0cdbfe494d6d4
HYEON <hyeon970315@gmail.com>
2024-08-16 10:55:08 +0900

    initial commit

---
+++ 01-자바스크립트-시작하기/HelloWorld.js
@@ -0,0 +1,15 @@
+console.group("MyMessage1");
+console.log("안녕하세요. Javascript1");
+console.log("안녕하세요. Javascript1");
+console.log("안녕하세요. Javascript1");
+console.groupEnd();
+
+
+console.group("mymessage2");
+console.log("안녕하세요");
+
+console.group("mymessage2-1");
+console.log("hi");
+console.log("heello");
+console.groupEnd();
+console.groupEnd();
